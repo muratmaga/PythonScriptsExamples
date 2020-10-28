60dba8a8f740060614f869e599c8ab60962caa25
muratmaga <muratmaga@outlook.com>
2020-10-28 12:50:15 -0700

    Create SA_from_closed_curve.py

---
+++ SA_from_closed_curve.py
@@ -0,0 +1,6 @@
+
+#assumes a single closed curve node
+curve=slicer.util.getNodesByClass("vtkMRMLMarkupsClosedCurveNode")[0]
+crossSectionSurface = vtk.vtkPolyData()
+areaMm2 = slicer.modules.markups.logic().GetClosedCurveSurfaceArea(curve, crossSectionSurface)
+print("Curve {0}: surface area = {1:.2f} mm2".format(curve.GetName(), areaMm2))
